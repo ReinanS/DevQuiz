7ae0fde8d2336f5bc6ba342b9375a733ec0a298e
reinanS <reinandesouza01@gmail.com>
2021-04-25 13:59:13 -0300

    feat: Challenge Page

---
+++ lib/challenge/challenge_page.dart
@@ -0,0 +1,26 @@
+import 'package:dev_quiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
+import 'package:dev_quiz/challenge/widgets/quiz/quiz_widget.dart';
+import 'package:flutter/material.dart';
+
+class ChallengPage extends StatefulWidget {
+  @override
+  _ChallengPageState createState() => _ChallengPageState();
+}
+
+class _ChallengPageState extends State<ChallengPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: SafeArea(
+          top: true,
+          child: QuestionIndicatorWidget(),
+        ),
+      ),
+      body: QuizWidget(
+        title: "O que o Flutter faz em sua totalidade?",
+      ),
+    );
+  }
+}
---
+++ lib/challenge/widgets/question_indicator/question_indicator_widget.dart
@@ -0,0 +1,27 @@
+import 'package:dev_quiz/core/core.dart';
+import 'package:dev_quiz/shared/widgets/linear_progress_indicator_widget.dart';
+import 'package:flutter/material.dart';
+
+class QuestionIndicatorWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Questão 04", style: AppTextStyles.body),
+              Text("de 10", style: AppTextStyles.body),
+            ],
+          ),
+          SizedBox(height: 16),
+          LinearProgressIndicatorWidget(
+            value: 0.7,
+          ),
+        ],
+      ),
+    );
+  }
+}
