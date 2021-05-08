cabfd8931db674026ca2b7a7357999626fa22023
reinanS <reinandesouza01@gmail.com>
2021-05-08 16:13:35 -0300

    feat: adiciona pageController em ChallengePage e permite navegação entre várias telas

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,26 +1,70 @@
+import 'package:dev_quiz/challenge/challenge_controller.dart';
+import 'package:flutter/material.dart';
+
 import 'package:dev_quiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:dev_quiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:dev_quiz/challenge/widgets/quiz/quiz_widget.dart';
-import 'package:flutter/material.dart';
+import 'package:dev_quiz/shared/models/question_model.dart';
 
-class ChallengPage extends StatefulWidget {
+class ChallengePage extends StatefulWidget {
+  final List<QuestionModel> questions;
+
+  const ChallengePage({
+    Key? key,
+    required this.questions,
+  }) : super(key: key);
+
   @override
-  _ChallengPageState createState() => _ChallengPageState();
+  _ChallengePageState createState() => _ChallengePageState();
 }
 
-class _ChallengPageState extends State<ChallengPage> {
+class _ChallengePageState extends State<ChallengePage> {
+  final controller = ChallengeController();
+  final pageController = PageController();
+
+  @override
+  void initState() {
+    pageController.addListener(() {
+      controller.currentPage = pageController.page!.toInt() + 1;
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(60),
+        preferredSize: Size.fromHeight(86),
         child: SafeArea(
           top: true,
-          child: QuestionIndicatorWidget(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  }),
+              // Faz com que renderize novamente somente este trecho de código
+              ValueListenableBuilder<int>(
+                valueListenable: controller.currentPageNotifier,
+                builder: (context, value, _) => QuestionIndicatorWidget(
+                  currentPage: value,
+                  length: widget.questions.length,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      body: QuizWidget(
-        title: "O que o Flutter faz em sua totalidade?",
+      body: PageView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: pageController,
+        children: widget.questions
+            .map((e) => QuizWidget(
+                  question: e,
+                ))
+            .toList(),
       ),
       bottomNavigationBar: SafeArea(
         bottom: true,
@@ -32,7 +76,11 @@ class _ChallengPageState extends State<ChallengPage> {
               Expanded(
                   child: NextButtonWidget.white(
                 label: "Pular",
-                onTap: () {},
+                onTap: () {
+                  pageController.nextPage(
+                      duration: Duration(milliseconds: 100),
+                      curve: Curves.linear);
+                },
               )),
               SizedBox(width: 7),
               Expanded(
