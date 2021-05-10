1e257475a1cd80a5af55ac9070caa76d381749da
reinanS <reinandesouza01@gmail.com>
2021-05-10 15:31:40 -0300

    ref: modifica a implementacao da lista de widgets em QuizWidget

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,14 +1,29 @@
 import 'package:dev_quiz/challenge/widgets/answer/answer_widget.dart';
 import 'package:dev_quiz/core/core.dart';
+import 'package:dev_quiz/shared/models/answer_model.dart';
 import 'package:dev_quiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
-class QuizWidget extends StatelessWidget {
+// Tarefa extra:
+// Implementar bloqueio ao selecionar uma alternativa
+
+class QuizWidget extends StatefulWidget {
   final QuestionModel question;
+  final VoidCallback onChange;
 
   QuizWidget({
     required this.question,
+    required this.onChange,
   });
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+
+  AnswerModel answer(int index) => widget.question.answers[index];
 
   @override
   Widget build(BuildContext context) {
@@ -18,19 +33,33 @@ class QuizWidget extends StatelessWidget {
         children: [
           SizedBox(height: 30),
           Text(
-            question.title,
+            widget.question.title,
             style: AppTextStyles.heading,
           ),
           SizedBox(height: 24),
+          for (int i = 0; i < widget.question.answers.length; i++)
+            AnswerWidget(
+              answer: answer(i),
+              disabled: indexSelected != -1,
+              isSelected: indexSelected == i,
+              onTap: () {
+                indexSelected = i;
+
+                setState(() {});
+                Future.delayed(Duration(seconds: 1))
+                    .then((value) => widget.onChange());
+              },
+            ),
+
           // Uso de operador de screed. Concatena a lista
-          ...question.answers
-              .map(
-                (e) => AnswerWidget(
-                  title: e.title,
-                  isRight: e.isRight,
-                ),
-              )
-              .toList(),
+          // ...widget.question.answers
+          //     .map(
+          //       (e) => AnswerWidget(
+          //         title: e.title,
+          //         isRight: e.isRight,
+          //       ),
+          //     )
+          //     .toList(),
         ],
       ),
     );
