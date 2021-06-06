372bee0e156c650e44cafcb7d3ab22652bd46d25
reinanS <reinandesouza01@gmail.com>
2021-06-06 13:14:26 -0300

    ref: torna o label de LevelButton um final

--- lib/home/widgets/level_button/level_button_widget.dart
+++ lib/home/widgets/level_button/level_button_widget.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class LevelButtonWidget extends StatelessWidget {
-  String label;
+  final String label;
 
   LevelButtonWidget({
     required this.label,
