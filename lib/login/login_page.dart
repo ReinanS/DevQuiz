bfd4036e5dcc34eb8b18cf6b7dd759a270953059
reinanS <reinandesouza01@gmail.com>
2021-06-11 19:53:42 -0300

    feat: adiciona logo e responsividade ao login

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:dev_quiz/core/core.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -8,6 +9,25 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size deviceSize = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: buildBody(deviceSize),
+    );
+  }
+
+  SingleChildScrollView buildBody(Size deviceSize) {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            AppImages.colorfulLogo,
+            height: deviceSize.height * 0.3,
+          ),
+        ],
+      ),
+    );
   }
 }
