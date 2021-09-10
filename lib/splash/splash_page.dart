198c5968c09b24c48ea6f76e75d00edb498659ce
reinanS <reinandesouza01@gmail.com>
2021-09-09 21:04:20 -0300

    ref: ajuste de direcionamento da splash screen

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,5 +1,6 @@
 import 'package:dev_quiz/core/core.dart';
 import 'package:dev_quiz/home/home_page.dart';
+import 'package:dev_quiz/login/login_page.dart';
 import 'package:dev_quiz/welcome/welcome_page.dart';
 import 'package:dev_quiz/right/right_page.dart';
 import 'package:flutter/material.dart';
@@ -12,7 +13,7 @@ class SplashPage extends StatelessWidget {
         context,
         MaterialPageRoute(builder: (context) {
           // return HomePage();
-          return WelcomePage();
+          return LoginPage();
         }),
       ),
     );
