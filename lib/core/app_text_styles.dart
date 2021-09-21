91c08483fd9f5cf36efa051fc6a639736b15ab7a
reinanS <reinandesouza01@gmail.com>
2021-09-21 15:32:05 -0300

    feat: adiciona outros estilos

--- lib/core/app_text_styles.dart
+++ lib/core/app_text_styles.dart
@@ -28,6 +28,12 @@ class AppTextStyles {
     fontWeight: FontWeight.w600,
   );
 
+  static final TextStyle heading30 = GoogleFonts.notoSans(
+    color: AppColors.black,
+    fontSize: 30,
+    fontWeight: FontWeight.w700,
+  );
+
   static final TextStyle heading40 = GoogleFonts.notoSans(
     color: AppColors.black,
     fontSize: 40,
@@ -69,11 +75,18 @@ class AppTextStyles {
     fontWeight: FontWeight.w500,
   );
 
+  static final TextStyle body15 = GoogleFonts.notoSans(
+    color: AppColors.grey,
+    fontSize: 15,
+    fontWeight: FontWeight.normal,
+  );
+
   static final TextStyle body20 = GoogleFonts.notoSans(
     color: AppColors.grey,
     fontSize: 20,
     fontWeight: FontWeight.normal,
   );
+
   static final TextStyle bodyLightGrey20 = GoogleFonts.notoSans(
     color: AppColors.lightGrey,
     fontSize: 20,
