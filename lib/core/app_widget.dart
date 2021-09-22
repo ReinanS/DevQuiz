3615cad4c2ae22a08df692cbbedadd45ea052f7e
reinanS <reinandesouza01@gmail.com>
2021-09-22 13:25:48 -0300

    ref: app widget, aplicacao de Provider

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,9 +1,22 @@
-import 'package:dev_quiz/splash/splash_page.dart';
+import 'package:dev_quiz/routers/routers.dart';
+import 'package:dev_quiz/views/settings/settings_controller.dart';
+import 'package:dev_quiz/views/splash/splash_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SplashPage();
+    return Provider<SettingsController>(
+      create: (context) => SettingsController(),
+      builder: (context, _) => MaterialApp(
+        title: "DevQuiz",
+        debugShowCheckedModeBanner: false,
+        // home: SettingsPage(),
+        initialRoute: "/",
+        onGenerateRoute: AppRouter.generateRoute,
+        theme: context.watch<SettingsController>().currentAppTheme,
+      ),
+    );
   }
 }
