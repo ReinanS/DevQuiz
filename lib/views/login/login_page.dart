d25bb5defd5a33dd030410ac4e29018d064a1059
reinanS <reinandesouza01@gmail.com>
2021-09-25 23:06:36 -0300

    ref: altera rota do botão de login para a home page

--- lib/views/login/login_page.dart
+++ lib/views/login/login_page.dart
@@ -104,7 +104,7 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   void _loginSign() async {
-    await Navigator.of(context).pushNamed(AppRoutes.settingsRoute);
+    await Navigator.of(context).pushNamed(AppRoutes.homeRoute);
     // await Navigator.of(context).pushNamed(AppRoutes.homeRoute);
   }
 }
