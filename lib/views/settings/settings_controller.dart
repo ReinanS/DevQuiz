12cf4570895d7266d9ceff7a70f9cd9b6c553cd5
reinanS <reinandesouza01@gmail.com>
2021-09-25 23:05:56 -0300

    ref: adiciona lógica de esolha do thema

--- lib/views/settings/settings_controller.dart
+++ lib/views/settings/settings_controller.dart
@@ -8,31 +8,62 @@ class SettingsController {
   ThemeData get currentAppTheme => themeNotifier.value;
   set currentAppTheme(ThemeData value) => themeNotifier.value = value;
 
-  void changeCurrentAppTheme({String? theme}) async {
+  void changeCurrentAppTheme(SelectedTheme theme, BuildContext context) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
 
-    if (theme != null) {
-      if (theme.toLowerCase() == "dark") {
+    switch (theme) {
+      case SelectedTheme.System:
+        _setSystemTheme(prefs, context);
+        break;
+
+      case SelectedTheme.Dark:
         _setDarkTheme(prefs);
-      } else {
+        break;
+
+      default:
         _setLightTheme(prefs);
-      }
-    } else {
-      if (currentAppTheme == AppTheme.lightTheme) {
-        _setDarkTheme(prefs);
-      } else if (currentAppTheme == AppTheme.darkTheme) {
-        _setLightTheme(prefs);
-      }
     }
+  }
+
+  void _setSystemTheme(SharedPreferences prefs, BuildContext context) {
+    if (brightness(context) == Brightness.dark) {
+      currentAppTheme = AppTheme.darkTheme;
+    } else {
+      currentAppTheme = AppTheme.lightTheme;
+    }
+
+    prefs.setInt("theme", 0);
   }
 
   void _setDarkTheme(SharedPreferences prefs) {
     currentAppTheme = AppTheme.darkTheme;
-    prefs.setString("theme", "dark");
+    prefs.setInt("theme", 1);
   }
 
   void _setLightTheme(SharedPreferences prefs) {
     currentAppTheme = AppTheme.lightTheme;
-    prefs.setString("theme", "light");
+    prefs.setInt("theme", 2);
+  }
+
+  Brightness brightness(BuildContext context) =>
+      MediaQuery.platformBrightnessOf(context);
+
+  // getCurrentBrightness() {
+  //   return WidgetsBinding.instance!.window.platformBrightness;
+  // }
+
+  SelectedTheme getTheme(int? theme) {
+    switch (theme) {
+      case 0:
+        return SelectedTheme.System;
+
+      case 1:
+        return SelectedTheme.Dark;
+
+      default:
+        return SelectedTheme.Light;
+    }
   }
 }
+
+enum SelectedTheme { System, Dark, Light }
