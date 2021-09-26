e061d1a77283ca2db212a6b7e86c9d9b984c4bd7
reinanS <reinandesouza01@gmail.com>
2021-09-25 23:01:31 -0300

    feat: criacao do custom radio tile

---
+++ lib/views/settings/widgets/custom_radio_list_tile.dart
@@ -0,0 +1,41 @@
+import 'package:dev_quiz/views/settings/settings_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CustomRadioListTile extends StatefulWidget {
+  final String title;
+  final String? subtitle;
+  final SelectedTheme value;
+  final SelectedTheme groupValue;
+  final Function(SelectedTheme?)? onChanged;
+
+  CustomRadioListTile({
+    required this.title,
+    this.subtitle,
+    required this.value,
+    required this.groupValue,
+    required this.onChanged,
+  });
+
+  @override
+  _CustomRadioListTileState createState() => _CustomRadioListTileState();
+}
+
+class _CustomRadioListTileState extends State<CustomRadioListTile> {
+  @override
+  Widget build(BuildContext context) {
+    SettingsController settingsController =
+        Provider.of<SettingsController>(context);
+
+    return RadioListTile<SelectedTheme>(
+        title: Text(
+          widget.title,
+          style:
+              TextStyle(color: settingsController.currentAppTheme.primaryColor),
+        ),
+        subtitle: widget.subtitle != null ? Text(widget.subtitle!) : null,
+        value: widget.value,
+        groupValue: widget.groupValue,
+        onChanged: widget.onChanged);
+  }
+}
