f70d4d7ac36b7465bb1d2f4b56e819804ef36880
reinanS <reinandesouza01@gmail.com>
2021-10-01 15:04:09 -0300

    ref: uso do AnimatedBuilder para gerência do thema

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,20 +1,21 @@
+import 'package:dev_quiz/core/app_theme.dart';
 import 'package:dev_quiz/routers/routers.dart';
 import 'package:dev_quiz/views/settings/settings_controller.dart';
-import 'package:dev_quiz/views/splash/splash_page.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Provider<SettingsController>(
-      create: (context) => SettingsController(),
+    return AnimatedBuilder(
+      animation: SettingsController.instance,
       builder: (context, _) => MaterialApp(
         title: "DevQuiz",
         debugShowCheckedModeBanner: false,
         initialRoute: "/",
         onGenerateRoute: AppRouter.generateRoute,
-        theme: context.watch<SettingsController>().currentAppTheme,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        themeMode: SettingsController.instance.currentThemeMode,
       ),
     );
   }
