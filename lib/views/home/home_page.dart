6d404a3e0276ce337b7aa064ebd40f7f4103a362
reinanS <reinandesouza01@gmail.com>
2021-10-12 21:10:21 -0300

    feat: inicio da lógica relacionada a filtragem

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -6,6 +6,7 @@ import 'package:dev_quiz/views/home/widgets/appbar/app_bar_widget.dart';
 import 'package:dev_quiz/views/home/widgets/level_button/level_button_widget.dart';
 import 'package:dev_quiz/views/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:dev_quiz/views/settings/settings_controller.dart';
+import 'package:dev_quiz/views/shared/models/quiz_model.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -30,7 +31,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-    final List categories = ['Fácil', 'Médio', 'Difícil', 'Perito'];
+    final List categories = ['Todos', 'Fácil', 'Médio', 'Difícil', 'Perito'];
     int selectedIndex = 0;
 
     if (controller.state == HomeState.sucess) {
@@ -74,28 +75,26 @@ class _HomePageState extends State<HomePage> {
                     crossAxisSpacing: 16,
                     mainAxisSpacing: 16,
                     crossAxisCount: 2,
-                    children: controller.quizzes!
-                        .map(
-                          (e) => QuizCardWidget(
-                            title: e.title,
-                            img: e.image,
-                            completed:
-                                "${e.questionsAnswered}/${e.questions.length}",
-                            percent: e.questionsAnswered / e.questions.length,
-                            onTap: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => ChallengePage(
-                                      questions: e.questions,
-                                      title: e.title,
-                                    ),
-                                  ));
-                              // print("Funciona");
-                            },
-                          ),
-                        )
-                        .toList()),
+                    children: controller.quizzes!.map((e) {
+                      return QuizCardWidget(
+                        title: e.title,
+                        img: e.image,
+                        completed:
+                            "${e.questionsAnswered}/${e.questions.length}",
+                        percent: e.questionsAnswered / e.questions.length,
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ChallengePage(
+                                  questions: e.questions,
+                                  title: e.title,
+                                ),
+                              ));
+                          // print("Funciona");
+                        },
+                      );
+                    }).toList()),
               )
             ],
           ),
